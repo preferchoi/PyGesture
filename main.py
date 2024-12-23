815ad418a69567435a711a0669570f29956bae4c
prefer_choi <prefer_choi@naver.com>
2024-12-23 19:39:06 +0900

    feat: 오른손 관련 마우스 이벤트 리팩토링 및 코드 정리

--- main.py
+++ main.py
@@ -56,26 +56,64 @@ def __main__():
         results = hands.process(rgb_frame)
         if results.multi_hand_landmarks:
             for hand_landmarks, handedness in zip(results.multi_hand_landmarks, results.multi_handedness):
-                gum_gi = hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP]
-                gum_gi_x, gum_gi_y = int(gum_gi.x * screen_w), int(gum_gi.y * screen_h)
-
+                # 오른손(마우스)
                 if handedness.classification[0].label == 'Right':
-                    pyautogui.moveTo(gum_gi_x, gum_gi_y)
+                    # 검지
+                    index_finger_tip = hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP]
+                    # 엄지
+                    thumb_tip = hand_landmarks.landmark[mp_hands.HandLandmark.THUMB_TIP]
+                    # 중지
+                    middle_finger_tip = hand_landmarks.landmark[mp_hands.HandLandmark.MIDDLE_FINGER_TIP]
+                    # 약지
+                    ring_finger_tip = hand_landmarks.landmark[mp_hands.HandLandmark.RING_FINGER_TIP]
 
-                    if read_click:
-                        thumb_tib = hand_landmarks.landmark[mp_hands.HandLandmark.THUMB_TIP]
-                        thumb_ip = hand_landmarks.landmark[mp_hands.HandLandmark.THUMB_IP]
+                    # 마우스 이동을 위한 검지 좌표 정규화 값
+                    index_finger_tip_x = int(index_finger_tip.x * screen_w)
+                    index_finger_tip_y = int(index_finger_tip.y * screen_h)
 
-                        thumb_tib_X = int(thumb_tib.x * screen_w)
-                        thumb_ip_X = int(thumb_ip.x * screen_w)
+                    # 마우스 이동 이벤트(상시 작동)
+                    pyautogui.moveTo(index_finger_tip_x, index_finger_tip_y)
 
-                        if thumb_ip_X < thumb_tib_X:
-                            cv2.circle(frame, (0, 0), 100, (0, 255, 0), -1)
-                            pyautogui.rightClick()
+                    # 좌클릭 이벤트를 위한 엄지 좌표 값 + 중지 좌표 값
+                    thumb_tip_x = int(thumb_tip.x * screen_w)
+                    thumb_tip_y = int(thumb_tip.y * screen_h)
+                    middle_finger_tip_x = int(middle_finger_tip.x * screen_w)
+                    middle_finger_tip_y = int(middle_finger_tip.y * screen_h)
 
+                    distance = ((thumb_tip_x - middle_finger_tip_x) ** 2 + (
+                                thumb_tip_y - middle_finger_tip_y) ** 2) ** 0.5
+
+                    if distance < 50:
+                        pyautogui.leftClick()
+
+                    # 우클릭 이벤트를 위한 약지 좌표 값
+                    ring_finger_tip_x = int(ring_finger_tip.x * screen_w)
+                    ring_finger_tip_y = int(ring_finger_tip.y * screen_h)
+
+                    distance = ((thumb_tip_x - ring_finger_tip_x) ** 2 + (thumb_tip_y - ring_finger_tip_y) ** 2) ** 0.5
+
+                    if distance < 50:
+                        pyautogui.rightClick()
+
+                # 왼손(키보드)
                 elif handedness.classification[0].label == 'Left':
-                    pass
 
+                    '''
+                    type: 사용하는 손가락 수에 따른 타입
+                    point_1: 기준점이 되는 포인트
+                    point_2: 이동하는 포인트
+                    input: 입력해야 하는 이벤트
+                    output: 출력해야 하는 이벤트
+                    detail: 출력하는 이벤트의 상세 정보
+                    
+                    type: '1 finger'|'2 finger'|'3 finger'|'4 finger'|'5 finger'
+                    point_1: [str]
+                    point_2: [str]
+                    input: ['cross' | 'over' | 'under']
+                    output: 'press' | 'click' | 'hold'
+                    detail: 'right' | 'left' | keyboard_key
+                    '''
+                    pass
                 mp_draw.draw_landmarks(frame, hand_landmarks, mp_hands.HAND_CONNECTIONS)
 
         cv2.imshow('camera', frame)
