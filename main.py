b7151dd1679ff14767db72f30fcfb6f314220717
prefer_choi <prefer_choi@naver.com>
2024-12-23 09:49:06 +0900

    feat: 손 두 개 이상 검출 가능하게 처리

--- main.py
+++ main.py
@@ -57,41 +57,41 @@ def __main__():
         rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         results = hands.process(rgb_frame)
         if results.multi_hand_landmarks:
-            hand_landmarks = results.multi_hand_landmarks[-1]
-            gum_gi = hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP]
-            jung_ji = hand_landmarks.landmark[mp_hands.HandLandmark.MIDDLE_FINGER_TIP]
+            for hand_landmarks, handedness in zip(results.multi_hand_landmarks, results.multi_handedness):
+                gum_gi = hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP]
+                jung_ji = hand_landmarks.landmark[mp_hands.HandLandmark.MIDDLE_FINGER_TIP]
 
-            gum_gi_x, gum_gi_y = int(gum_gi.x * screen_w), int(gum_gi.y * screen_h)
-            jung_ji_x, jung_ji_y = int(jung_ji.x * screen_w), int(jung_ji.y * screen_h)
+                gum_gi_x, gum_gi_y = int(gum_gi.x * screen_w), int(gum_gi.y * screen_h)
+                jung_ji_x, jung_ji_y = int(jung_ji.x * screen_w), int(jung_ji.y * screen_h)
 
-            frame_gum_gi_x, frame_gum_gi_y = int(gum_gi.x * frame_w), int(gum_gi.y * frame_h)
+                frame_gum_gi_x, frame_gum_gi_y = int(gum_gi.x * frame_w), int(gum_gi.y * frame_h)
 
-            distance = ((gum_gi_x - jung_ji_x) ** 2 + (gum_gi_y - jung_ji_y) ** 2) ** 0.5
+                distance = ((gum_gi_x - jung_ji_x) ** 2 + (gum_gi_y - jung_ji_y) ** 2) ** 0.5
 
-            if distance < 50:
-                cv2.circle(frame, (frame_gum_gi_x, frame_gum_gi_y), 100, (0, 255, 0), -1)
-                pyautogui.moveTo(gum_gi_x, gum_gi_y)
+                if distance < 50:
+                    cv2.circle(frame, (frame_gum_gi_x, frame_gum_gi_y), 100, (0, 255, 0), -1)
+                    pyautogui.moveTo(gum_gi_x, gum_gi_y)
 
-            if read_click:
-                um_gi = hand_landmarks.landmark[mp_hands.HandLandmark.THUMB_TIP]
-                yak_ji = hand_landmarks.landmark[mp_hands.HandLandmark.RING_FINGER_TIP]
+                if read_click:
+                    um_gi = hand_landmarks.landmark[mp_hands.HandLandmark.THUMB_TIP]
+                    yak_ji = hand_landmarks.landmark[mp_hands.HandLandmark.RING_FINGER_TIP]
 
-                um_gi_x, um_gi_y = int(um_gi.x * screen_w), int(um_gi.y * screen_h)
-                yak_ji_x, yak_ji_y = int(yak_ji.x * screen_w), int(yak_ji.y * screen_h)
+                    um_gi_x, um_gi_y = int(um_gi.x * screen_w), int(um_gi.y * screen_h)
+                    yak_ji_x, yak_ji_y = int(yak_ji.x * screen_w), int(yak_ji.y * screen_h)
 
-                frame_um_gi_x, frame_um_gi_y = int(um_gi.x * frame_w), int(um_gi.y * frame_h)
+                    frame_um_gi_x, frame_um_gi_y = int(um_gi.x * frame_w), int(um_gi.y * frame_h)
 
-                click_distance = ((um_gi_x - yak_ji_x) ** 2 + (um_gi_y - yak_ji_y) ** 2) ** 0.5
-                if click_distance < 50:
-                    if read_click == ClickState.READY:
-                        cv2.circle(frame, (frame_um_gi_x, frame_um_gi_y), 100, (0, 255, 0), -1)
-                        pyautogui.click()
-                        read_click = ClickState.BUSY
+                    click_distance = ((um_gi_x - yak_ji_x) ** 2 + (um_gi_y - yak_ji_y) ** 2) ** 0.5
+                    if click_distance < 50:
+                        if read_click == ClickState.READY:
+                            cv2.circle(frame, (frame_um_gi_x, frame_um_gi_y), 100, (0, 255, 0), -1)
+                            pyautogui.click()
+                            read_click = ClickState.BUSY
 
-            elif read_click == ClickState.BUSY:
-                read_click = ClickState.READY
+                elif read_click == ClickState.BUSY:
+                    read_click = ClickState.READY
 
-            mp_draw.draw_landmarks(frame, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+                mp_draw.draw_landmarks(frame, hand_landmarks, mp_hands.HAND_CONNECTIONS)
 
         cv2.imshow('camera', frame)
         if cv2.waitKey(1) != -1:
